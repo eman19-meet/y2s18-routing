65bbd37e8ca33b6236db9b14dc188c4ea49620b3
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 16:22:40 +0300

    dfghjk

--- lab/app.py
+++ lab/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/student/<int:student_id>')
+@app.route('/student/<int:sid>')
 def display_student(student_id):
-    return render_template('student.html', student_id=student_id)
+    return render_template('student.html', student=query_by_id(student_id))
 
 app.run(debug=True)
